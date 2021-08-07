1777f12b2cf8c8750e2097573d53a9434ffb03b0
Vedran Bilopavlović <vbilopav@gmail.com>
2021-08-07 10:54:44 +0200

    add loading screen to a spa example

--- RazorSvelte/Pages/SpaExample.cshtml.ts
+++ RazorSvelte/Pages/SpaExample.cshtml.ts
@@ -1,4 +1,5 @@
 /// <reference types="svelte" />
 import App from "../App/spa-example.svelte";
 import "bootstrap/js/src/collapse";
-export default new App({target: document.body});
+document.getElementById("loading")?.remove();
+export default  new App({target: document.body});
