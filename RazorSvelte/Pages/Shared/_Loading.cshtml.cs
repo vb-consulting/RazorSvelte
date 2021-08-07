77c7b28d94dbd87de7f1a0145347e8000ed4304f
Vedran Bilopavlović <vbilopav@gmail.com>
2021-08-07 11:25:08 +0200

    move loading markup to partial view component

---
+++ RazorSvelte/Pages/Shared/_Loading.cshtml.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorSvelte.Pages.Shared
+{
+    public class Loading
+    {
+        public string Id { get; init; } = "loading";
+        public string MarginTop { get; init; } = "100px";
+        public string LoadingClass { get; init; } = "spinner-grow";
+        public string Size { get; init; } = "10rem";
+    }
+}
