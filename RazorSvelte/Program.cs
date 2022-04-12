59e2bb22d6e71399b2ae7bb026e0cb920c07b379
Vedran Bilopavlović <vbilopav@gmail.com>
2022-04-12 13:46:41 +0200

    cleanr program.cs

--- RazorSvelte/Program.cs
+++ RazorSvelte/Program.cs
@@ -13,18 +13,15 @@ using Microsoft.AspNetCore.Localization;
 JsonConvert.DefaultSettings = () => new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient().AddOptions();
 var authBuilder = new AuthBuilder(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler(Consts.ErrorUrl);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -34,40 +31,49 @@ app.UseRouting();
 authBuilder.BuildAuth(app);
 app.UseAuthorization();
 app.MapRazorPages();
-app.MapFallback(context =>
-{
-    if (!context.Request.Path.StartsWithSegments(Consts.ApiSegment))
-    {
-        context.Response.Redirect(Consts.NotFoundUrl);
-    }
-    return Task.CompletedTask;
-});
-
-var localizationEnabled = builder.Configuration.GetValue<bool>("EnableBrowserRequestLocalization");
-if (localizationEnabled is true)
-{
-    var defaultCulture = builder.Configuration.GetValue<string>("DefaultCulture") ?? "en-US";
-    var supportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures).Where(cul => !string.IsNullOrEmpty(cul.Name)).ToList();
-    var localizationOptions = new RequestLocalizationOptions
-    {
-        ApplyCurrentCultureToResponseHeaders = true,
-        DefaultRequestCulture = new RequestCulture(defaultCulture),
-        SupportedCultures = supportedCultures,
-        SupportedUICultures = supportedCultures,
-        RequestCultureProviders = new List<IRequestCultureProvider>
-        {
-            new CustomRequestCultureProvider(ctx =>
-            {
-                var lang = ctx.Request.Headers["Accept-Language"].ToString().Split(',').FirstOrDefault();
-                var culture = string.IsNullOrEmpty(lang) ? defaultCulture : lang;
-                ProviderCultureResult result = new(culture, culture);
-                return Task.FromResult((ProviderCultureResult?)result);
-            })
-        }
-    };
-}
+MapFallback(app);
+ConfigureLocalization(builder.Configuration);
 
 app.Run();
+
+static void MapFallback(WebApplication app)
+{
+    app.MapFallback(context =>
+    {
+        if (!context.Request.Path.StartsWithSegments(Consts.ApiSegment))
+        {
+            context.Response.Redirect(Consts.NotFoundUrl);
+        }
+        return Task.CompletedTask;
+    });
+}
+
+static void ConfigureLocalization(ConfigurationManager configuration)
+{
+    var localizationEnabled = configuration.GetValue<bool>("EnableBrowserRequestLocalization");
+    if (localizationEnabled is true)
+    {
+        var defaultCulture = configuration.GetValue<string>("DefaultCulture") ?? "en-US";
+        var supportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures).Where(cul => !string.IsNullOrEmpty(cul.Name)).ToList();
+        var localizationOptions = new RequestLocalizationOptions
+        {
+            ApplyCurrentCultureToResponseHeaders = true,
+            DefaultRequestCulture = new RequestCulture(defaultCulture),
+            SupportedCultures = supportedCultures,
+            SupportedUICultures = supportedCultures,
+            RequestCultureProviders = new List<IRequestCultureProvider>
+            {
+                new CustomRequestCultureProvider(ctx =>
+                {
+                    var lang = ctx.Request.Headers["Accept-Language"].ToString().Split(',').FirstOrDefault();
+                    var culture = string.IsNullOrEmpty(lang) ? defaultCulture : lang;
+                    ProviderCultureResult result = new(culture, culture);
+                    return Task.FromResult((ProviderCultureResult?)result);
+                })
+            }
+        };
+    }
+}
 
 #pragma warning disable CA1050 // Declare types in namespaces
 public class Consts
